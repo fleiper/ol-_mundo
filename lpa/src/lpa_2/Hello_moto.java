091964ec78c257a4e04477399e63c523af4b7114
fleiper <estudoinglesisso@gmailcom>
2024-03-06 10:55:13 -0300

    primeira versão do ola mundo

---
+++ lpa/src/lpa_2/Hello_moto.java
@@ -0,0 +1,33 @@
+package lpa_2;
+//import java.util.Scanner
+public class Hello_moto {
+
+	public static void main(String[] args) {
+		System.out.println("hello moto turururu t was late at night\r\n"
+				+ "You held on tight\r\n"
+				+ "From an empty seat\r\n"
+				+ "A flash of light\r\n"
+				+ "\r\n"
+				+ "It will take a while\r\n"
+				+ "To make you smile\r\n"
+				+ "Somewhere in these eyes\r\n"
+				+ "I’m on your side\r\n"
+				+ "\r\n"
+				+ "You wide-eyed girls\r\n"
+				+ "You get it right\r\n"
+				+ "\r\n"
+				+ "Fall back into place\r\n"
+				+ "Fall back into place\r\n"
+				+ "\r\n"
+				+ "Tender is the night\r\n"
+				+ "For a broken heart\r\n"
+				+ "Who will dry your eyes\r\n"
+				+ "When it falls apart?\r\n"
+				+ "\r\n"
+				+ "What makes this fragile world go 'round?\r\n"
+				+ "Were you ever lost?\r\n"
+				+ "Was she ever found?");
+		
+	}
+
+}
